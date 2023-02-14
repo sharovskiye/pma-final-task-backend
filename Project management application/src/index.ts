2fbef6361c84535caa4ab09e775f51fa88de96b8
Yevhenii <sharovskiyevgeniy@gmail.com>
2023-02-14 20:22:22 +0200

    add mongodb in env

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}');
+    await mongoose.connect(process.env.MONGODB);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
